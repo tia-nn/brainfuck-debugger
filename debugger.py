6ebdb1cdca0136804a37b60738c651deeada4cfc
tia <yknms3533642@yahoo.co.jp>
2019-11-09 12:25:36 +0900

    add snap command

--- debugger.py
+++ debugger.py
@@ -1,9 +1,11 @@
-from typing import List, Set
+from typing import List, Set, Union
 import os
 import sys
 from itertools import zip_longest
 from string import whitespace
 from collections import defaultdict
+from copy import deepcopy
+from dataclasses import dataclass
 
 from machine import BrainFuckMachine
 from color import COLOR
@@ -11,9 +13,17 @@ from color import COLOR
 
 class Debugger:
     machine: BrainFuckMachine
-    snaps: List[BrainFuckMachine]
+    snaps: List['Snap']
     breakpoints: Set[int]
     is_ended: bool
+
+    @dataclass
+    class Snap:
+        snap: BrainFuckMachine
+        comment: str
+
+        def __repr__(self):
+            return str(self.comment)
 
     def __init__(self, code):
         self.machine = BrainFuckMachine(False)
@@ -30,6 +40,26 @@ class Debugger:
     def remove_break(self, p):
         self.breakpoints.remove(p)
 
+    def save_snap(self, comment: str = None):
+        comment = comment or 'snap at %s' % self.machine.ip
+        snap = self.Snap(deepcopy(self.machine), comment)
+        self.snaps.append(snap)
+
+    def load_snap(self, snap: Union[int, BrainFuckMachine, Snap]):
+        if isinstance(snap, int):
+            machine = self.snaps[snap].snap
+
+        elif isinstance(snap, BrainFuckMachine):
+            machine = snap
+
+        elif isinstance(snap, self.Snap):
+            machine = snap.snap
+
+        else:
+            return True
+
+        self.machine = deepcopy(machine)
+
     def step(self):
         self.is_ended = self.machine.step()
         return self.is_ended
@@ -40,45 +70,50 @@ class Debugger:
         for i in whitespace:
             code = code.replace(i, ' ')
 
-        put_str = []
+        put_str_code = []
         for i, v in enumerate([code[i:i + 10*n] for i in range(0, len(code), 10*n)]):
             if i == machine.ip // (10*n):
                 a = machine.ip % (10*n)
-                put_str.append(('{:%d}' % (11*n-1)).format(' ' * (a + a // 10) + 'v'))
+                put_str_code.append(('{:%d}' % (11*n-1)).format(' ' * (a + a // 10) + 'v'))
             else:
-                put_str.append(' ' * (11*n-1))
+                put_str_code.append(' ' * (11*n-1))
 
             b = ''
             for i in range(0, 10*n, 10):
                 b += v[i:i+10] + ' '
-            put_str.append(('{:%d}' % (11*n-1)).format(b))
-            put_str.append('0123456789 ' * n)
+            put_str_code.append(('{:%d}' % (11*n-1)).format(b))
+            put_str_code.append('0123456789 ' * n)
 
-        put_str.append('')
-
-        put_str.append('array:      %s' % machine.array)
-        put_str.append('pointer:    %s' % machine.pointer)
-        put_str.append('stack:      %s' % machine.stack)
-        put_str.append('ip:         %s' % machine.ip)
-
-        put_str.append('')
-        put_str.append('breaks:     %s' % self.breakpoints)
-        put_str.append('')
-
-        put_str.append('stdout:     %s' % machine.stdout)
-        put_str.append('stdin:      %s' % machine.stdin)
-        put_str.append('            %s' % (' ' * machine.stdin_p + 'A'))
-
-        os.system('clear')
         for i in sorted(self.breakpoints, key=lambda x: -x):
             row = i // (10 * n) * 3 + 1
             col = i % (10 * n)
             col += col // 10
-            s = put_str[row]
+            s = put_str_code[row]
             c = s[col]
-            s = s[:col] + COLOR.CYAN + c + COLOR.END + s[col+1:]
-            put_str[row] = s
+            s = s[:col] + COLOR.CYAN + c + COLOR.END + s[col + 1:]
+            put_str_code[row] = s
 
+
+        put_str_state = []
+
+        put_str_state.append('array:      %s' % machine.array)
+        put_str_state.append('pointer:    %s' % machine.pointer)
+        put_str_state.append('stack:      %s' % machine.stack)
+        put_str_state.append('ip:         %s' % machine.ip)
+
+        put_str_state.append('')
+        put_str_state.append('breaks:     %s' % self.breakpoints)
+        put_str_state.append('snaps:      %s' % self.snaps)
+        put_str_state.append('')
+
+        put_str_state.append('stdout:     %s' % machine.stdout)
+        put_str_state.append('stdin:      %s' % machine.stdin)
+        put_str_state.append('            %s' % (' ' * machine.stdin_p + 'A'))
+
+
+        put_str = put_str_code + [''] + put_str_state
+
+        os.system('clear')
         for i in put_str:
             print(i)
 
@@ -126,6 +161,14 @@ class Debugger:
                 if not a:
                     a = '\n'
                 self.machine.stdin += a
+
+            elif a[0] == 'v':
+                if len(a) == 1 or a[1] == 's':
+                    print('snap comment: ', end='')
+                    a = input()
+                    self.save_snap(a)
+                elif a[1] == 'l':
+                    self.load_snap(int(a[2:]))
 
             elif a[0] == '.':
                 return
